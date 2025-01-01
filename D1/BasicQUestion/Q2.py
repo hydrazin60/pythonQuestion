95ed66441dfbe9b0bcbf7f387f2d7a8c94886ae3
hydrazin60 <sureshkhdk45@gmail.com>
2025-01-01 18:55:26 +0545

    basic question and solution of Python

---
+++ D1/BasicQUestion/Q2.py
@@ -0,0 +1,15 @@
+# # Write a program to calculate the sum of all even numbers from 1 to 100.
+i = 0
+sum = 0
+while i < 10:
+    if i%2 == 0:
+        sum = sum + i
+    i = i+1
+print(sum)
+# for Loop
+sum = 0
+for i in range(1,10):
+    if(i%2 ==0 ):
+        sum = sum+i
+    i = i+1    
+print(sum)
---
+++ D1/BasicQUestion/loop.py
@@ -0,0 +1,5 @@
+# # Print numbers from 1 to 50 using a loop.
+i = 0
+while i <50:
+    print(i)
+    i +=1
